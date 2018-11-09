fc7898dcbf42569c42ae608d4bf44a91d2b3f748
Nick Scope <nscope52884@gmail.com>
2018-11-08 23:57:53 -0600

    Finished rough draft of header and footer along with made logo folder

--- Python/make_logo_transparent_light.py
+++ Python/make_logo_transparent_light.py
@@ -10,7 +10,7 @@ for item in datas:
     if item[0] >= threshold and item[1] >= threshold and item[2] >= threshold:
         newData.append((255, 255, 255, 0))
     else:
-        newData.append((0, 0, 0, 1))
+        newData.append((255, 255, 255, 255))
 
 img.putdata(newData)
 img.save(r'C:\Users\nscop\Documents\Projects\StatGalaxy\Logo_Rough_Draft_Transparent_Light.png', "PNG")
